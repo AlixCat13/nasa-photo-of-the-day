98e3e13a74a6e296fef723423587c095a2b4d759
Cody C <alix.henry@ymail.com>
2020-07-28 12:47:00 -0500

    Trying to get this project set up with the API and already having issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod')
+    .then( response => {
+        console.log(response)
+    })
+    .catch(error => console.log(error))
+    }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      {/* <NasaGrid /> */}
     </div>
   );
 }

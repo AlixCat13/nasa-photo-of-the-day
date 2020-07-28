4213cf519d3b8212279b901b9c7ba629d3196114
Cody C <alix.henry@ymail.com>
2020-07-28 13:38:21 -0500

    Have the data coming in. Just need to figure out how to access it correctly to display the photo of the day

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from 'axios'
 import "./App.css";
 
 function App() {
 
+  const [photo, setPhoto] = useState([])
+
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then( response => {
         console.log(response)
     })
@@ -14,7 +16,12 @@ function App() {
 
   return (
     <div className="App">
-      {/* <NasaGrid /> */}
+      <div className='photo-container'>
+         <h1 className='heading'>NASA Astronomy Photo of the Day</h1>
+            <h2 className='photo-title'>Photo Title</h2>
+               <img className='photo' src=''>Nasa Photo</img>
+
+      </div>
     </div>
   );
 }

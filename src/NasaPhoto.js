087308bc46b0396b4503e21ff5a7b9faae7e1014
Cody C <alix.henry@ymail.com>
2020-07-28 23:49:34 -0500

    Got the API working

--- src/NasaPhoto.js
+++ src/NasaPhoto.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const NasaPhoto = props => {
-    // return (
-    //     <div className='Nasa-card' key=''>
-    //       <img className='Nasa-image' alt='stars' src={props.image} />
-    //          <h2 class name='title'>Photo Title</h2>
-    //     </div>
-    // );
+    return (
+        <div className='Nasa-card' key=''>
+          <img className='Nasa-image' alt='stars' src={props.url} />
+             <h2 class name='title'>{props.title}</h2>
+        </div>
+    );
 }
 
 export default NasaPhoto

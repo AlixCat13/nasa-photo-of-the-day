908fdc140a82f228869f4a903ccfeaa7b6d31e7f
Cody C <alix.henry@ymail.com>
2020-07-28 21:17:21 -0500

    Trying to get the API to work again. And still getting things set up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, {useEffect, useState} from "react";
-import axios from 'axios'
+import React from "react";
 import "./App.css";
 
+
 function App() {
-
-  const [photo, setPhoto] = useState([])
-
-  useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then( response => {
-        console.log(response)
-    })
-    .catch(error => console.log(error))
-    }, [])
-
-  return (
-    <div className="App">
-      <div className='photo-container'>
-         <h1 className='heading'>NASA Astronomy Photo of the Day</h1>
-            <h2 className='photo-title'>Photo Title</h2>
-               <img className='photo' src=''>Nasa Photo</img>
-
-      </div>
-    </div>
-  );
+return (
+  <div className='App'>
+    <NasaCard />
+  </div>
+)
+ 
 }
 
 export default App;
---
+++ src/NasaPhoto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NasaPhoto = props => {
+    // return (
+    //     <div className='Nasa-card' key=''>
+    //       <img className='Nasa-image' alt='stars' src={props.image} />
+    //          <h2 class name='title'>Photo Title</h2>
+    //     </div>
+    // );
+}
+
+export default NasaPhoto

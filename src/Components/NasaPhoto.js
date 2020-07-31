02fcc0500f23eb57d3f9b71197f906a866b558f8
Cody C <alix.henry@ymail.com>
2020-07-31 12:40:22 -0500

    found the right logo to use

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -5,7 +5,7 @@ const NasaPhoto = ({ data }) => {
    
 return(
    <div className='Photo-Container'>
-       <img className='Nasa-Logo' alt='logo' src={require('../Photos/NasaLogo2.jpg')}/>
+       <img className='Nasa-Logo' alt='logo' src={require('../Photos/NasaLogo4.jpg')}/>
        <h1 className='Headline'>Nasa Astronomy Photo of the Day</h1>
           <h2 className='Photo-Title'>{data.title}</h2>
              <p className='date'>Today's Date is {data.date} </p>  
